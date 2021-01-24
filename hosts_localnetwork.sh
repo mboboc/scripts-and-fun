699f56ec9f575be1ce9a799aa2e2dc4f01f184f1
mboboc <madaeboboc@gmail.com>
2021-01-24 15:43:53 +0200

    update hosts_localnetwork.sh

--- hosts_localnetwork.sh
+++ hosts_localnetwork.sh
@@ -2,5 +2,11 @@
 
 # Find all the hosts connected to the specified network
 # Provide an ip with a mask as arguments
-
+# 192.168.0.0/24
+if [ $# -eq 0 ]
+  then
+    echo "Usage: ./hosts_localnetwork.sh 192.168.1.0/24"
+    echo "If this doesnt work, check your ip addres."
+    exit 1
+fi
 sudo nmap $1 -n -sP --disable-arp-ping | grep report | awk '{print $5}'
