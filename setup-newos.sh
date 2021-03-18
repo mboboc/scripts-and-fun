9a84239bb5046607958a08e00e6f212bbb424d04
mboboc <madaeboboc@gmail.com>
2021-03-18 15:11:56 +0200

    script for fast so2 tmux config

--- setup-newos.sh
+++ setup-newos.sh
@@ -1,8 +1,9 @@
-sudo apt-get install vim terminator ssh openssh-server make build-essential
+# Basic stuff
+sudo apt-get install vim terminator ssh openssh-server make build-essential git curl
+
+# Chrome
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo apt install ./google-chrome-stable_current_amd64.deb
-sudo apt install git
-sudo apt install curl
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
 sudo apt-get install apt-transport-https
 echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
@@ -12,5 +13,9 @@ curl -L https://go.microsoft.com/fwlink/p/?LinkID=2112886&clcid=0x418&culture=ro
 sudo dpkg -i teams_1.3.00.25560_amd64.deb
 wget https://downloads.slack-edge.com/linux_releases/slack-desktop-4.7.0-amd64.deb
 sudo dpkg -i slack-desktop-4.7.0-amd64.deb
-rm slack-desktop-4.7.0-amd64.deb
+
+# Discord
 sudo snap install discord
+
+# ZSH
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
---
+++ so2-tmux-session-maker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash                                                                                                 
+
+SESSIONNAME="so2"
+tmux has-session -t $SESSIONNAME &> /dev/null
+
+if [ $? != 0 ]
+then	
+	echo "Created a new SO2 Session"
+	cd /home/madix/linux
+	tmux new-session -s $SESSIONNAME -n 'repo' -d
+	
+	cd tools/labs
+	tmux new-window -t $SESSIONNAME -n 'tools-labs'
+	tmux new-window -t $SESSIONNAME -n 'dev' 	
+	tmux new-window -t $SESSIONNAME -n 'boot'
+	tmux new-window -t $SESSIONNAME -n 'minicom'
+fi
+
+tmux attach -t $SESSIONNAME
