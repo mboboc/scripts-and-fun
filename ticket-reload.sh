2661eb8f0a5062a0f653f194934f186ab3df7e08
mboboc <madaeboboc@gmail.com>
2022-03-18 14:02:01 +0200

    Improvements for ticket-reload.sh

--- ticket-reload.sh
+++ ticket-reload.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 # Usage: bash ticket-reload.sh
+wanted="16:00"
 while true; do
 	currenttime=$(date +%H:%M)
-	echo $currenttime
-	if [[ "$currenttime" == "16:00" ]]
+	echo "[$currenttime] Waiting for $wanted..."
+	if [[ "$currenttime" > "$wanted" ]]
+	then
+		echo "Time set is after the current time"
+		break
+	fi
+	if [[ "$currenttime" == "$wanted" ]]
 	then
 		xdg-open https://www.ticketmaster.es/event/ateez-tickets/30311?language=en-us
 		break
